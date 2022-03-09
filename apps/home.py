85244a975761d7515b4590619bb47429fc295b05
Mochamad Sina Sulaeman <mssulaeman@gmail.com>
2022-03-09 20:13:25 +0700

    add multiapp

--- apps/home.py
+++ apps/home.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
-st.header("Pengolahan Data Covid 19 Data Summary and Prediction - Study Case DKI Jakarta")
+import streamlit as st
 
-st.text()
+
+def app():
+    st.title('Home')
+
+    st.write('This is the `home page` of this multi-page app.')
+
+    st.header(
+        "Pengolahan Data Covid 19 Data Summary and Prediction - Study Case DKI Jakarta")
+
+    st.write(
+        'In this app, we will be building a simple classification model using the Iris dataset.')
