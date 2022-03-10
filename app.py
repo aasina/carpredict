eb8473b05c33f67f70ff079605111b50866751d5
Mochamad Sina Sulaeman <mssulaeman@gmail.com>
2022-03-10 16:07:31 +0700

    update home

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ app = MultiApp()
 app.add_app("Home", home.app)
 app.add_app("Graph", graph.app)
 app.add_app("Predict", predict.app)
+
 # The main app
 app.run()
-
-
-st.markdown("""
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
-""")
--- apps/home.py
+++ apps/home.py
@@ -3,4 +3,22 @@ import streamlit as st
 def app():
     st.title('Home')
 
-    st.write('Pengolahan Data Covid 19 Data Summary and Prediction - Study Case DKI Jakarta')
+    st.write('## Pengolahan Data Covid 19 Data Rekapitulasi and Prediction - Study Case DKI Jakarta')
+
+    st.write('')
+    st.write("""
+    ### Pertama-tama, disclaimer dulu ya.. :D
+
+    Website ini dibuat untuk keperluan latihan pengolahan data menggunakan bahasa pemrograman Python dan tidak diperuntukan sebagai rujukan dalam studi formal ataupun pembuatan kebijakan.
+    
+    ### Ada apa di web ini?
+
+    Web ini dibuat dalam bahasa pemrograman Python dengan beberapa library seperti Pandas, Scikit-Learn dan Streamlit.
+
+    Ada dua modul utama pada website ini, modul pertama adalah modul untuk menampilkan data-data yang didapat dalam bentuk grafik menggunakan library matplotlib dan seaborn.
+    Modul berikutnya adalah modul prediksi yang dibuat menggunakan scikit-learn berdasarkan Supervised Learning. Ada dua metoda yang digunakan yaitu metoda regresi dan klasifikasi.
+
+
+    ### Referensi
+    1. Streamlit multi-page app menggunakan [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework dikembangkan oleh [Praneel Nihar](https://medium.com/@u.praneel.nihar). Lampiran [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+    """)
