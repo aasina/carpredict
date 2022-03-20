98fd294aee85508ce8467232849bac5e379a2f67
Mochamad Sina Sulaeman <mssulaeman@gmail.com>
2022-03-20 11:53:44 +0700

    update home

--- apps/home.py
+++ apps/home.py
@@ -1,43 +1,54 @@
 import streamlit as st
+from PIL import Image
+
+image = Image.open("riskmodel.png")
 
 
 def app():
     st.title("Home")
 
-    st.write(
-        "## Pengolahan Data Covid 19 Data Rekapitulasi and Prediction - Study Case DKI Jakarta"
+    st.header(
+        "Pengolahan Data Covid 19 Data Rekapitulasi and Prediction - Study Case DKI Jakarta"
     )
+
+    st.subheader("Disclaimer")
 
     st.write(
         """
-    ### Pertama-tama, disclaimer dulu ya.. :D
-
-    Website ini dibuat untuk keperluan latihan pengolahan data menggunakan bahasa pemrograman Python dan tidak diperuntukan sebagai rujukan dalam studi formal ataupun pembuatan kebijakan.
+    1. Website ini dibuat untuk keperluan latihan pengolahan data menggunakan bahasa pemrograman Python dan tidak diperuntukan sebagai rujukan dalam studi formal ataupun pembuatan kebijakan.
+    2. Sumber dataset ada https://corona.jakarta.go.id/id dengan sedikit penyesuaian.
+    3. Model perhitungan Konsekuensi, Probabilitas Kegagalan adalah asumsi personal untuk tujuan pembelajaran.
     """
     )
 
-    st.subheader("Ada apa di web ini")
+    col1, col2 = st.columns(2)
 
-    with st.expander("Ada apa di web ini"):
-        st.write(
-            """
+    with col1:
+        st.subheader("Tentang Web")
+        with st.expander(""):
+            st.write(
+                """
             Web ini dibuat dalam bahasa pemrograman Python dengan beberapa library seperti Pandas, Scikit-Learn dan Streamlit.
 
             Ada dua modul utama pada website ini, modul pertama adalah modul untuk menampilkan data-data yang didapat dalam bentuk grafik menggunakan library matplotlib dan seaborn. Modul berikutnya adalah modul prediksi yang dibuat menggunakan scikit-learn berdasarkan Supervised Learning. Ada dua metoda yang digunakan yaitu metoda regresi dan klasifikasi.
-        """
-        )
-
-    st.subheader("Konsep")
-
-    with st.expander("Konsep Model"):
-        st.write(
             """
+            )
+
+    with col2:
+        st.subheader("Konsep Risiko")
+        with st.expander(""):
+            st.write(
+                """
             Konsep penilaian berbasis risiko umum digunakan pada industri untuk menilai kondisi ataupun kelayakan sebuah system ataupun peralatan. Risiko adalah perkalian atara Probabilitas Kegagalan dan Konsekuensi kegagalan.
+
+            Probabilitas kegagalan dinilai berdasarkan nilai positivity rate dan jumlah positive case number. Sedangkan konsekuensi dihitung berdasarkan jumlah kematian, jumlah pasien dirawat dan pasien isolasi mandiri.
 
             Risk (R) = Probability of Failure (PoF) x Consequence of Failure (CoF)
 
-        """
-        )
+            """
+            )
+
+    st.image(image, caption="3 x 3 Risk Model Assumption")
 
     st.subheader("Referensi")
     with st.expander("Referensi"):
