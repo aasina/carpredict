20fe3430d3261d2f1be03407e5023ef9f02e82d1
Mochamad Sina Sulaeman <mssulaeman@gmail.com>
2022-03-20 17:55:58 -0700

    dashboard

---
+++ dashboard.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#set wide page layout
+st.set_page_config(layout="wide")
+
+#Import Dataset
+dataset = pd.read_csv('datasetcovidsamplejkt.csv')
+
+ydayposid = dataset.ID_DAILY_POSITIVE
+ydayposjkt = dataset.JKT_DAILY_POSITIVE
+ydaydeathid = dataset.ID_DAILY_DEATH
+ydaydeathjkt = dataset.JKT_DAILY_DEATH
+xdaily = dataset.Tanggal
+
+#create first line
+with st.container():
+    col1, col2 = st.columns(2)
+
+    with col1:
+        fig1 = plt.figure()
+        plt.plot(xdaily,ydayposid,'r',label="Positive Harian Indonesia")
+        plt.plot(xdaily,ydayposjkt,'b',label="Positif Harian DKI Jakarta")
+        plt.xticks([1,150,300,450,600,730])
+        plt.xlabel('Tanggal')
+        plt.ylabel('Jumlah Positif Harian')
+        plt.legend()
+        st.pyplot(fig1)
+
+    with col2:
+        fig2 = plt.figure()
+        plt.plot(xdaily,ydaydeathid,'g',label="Meninggal Harian Indonesia")
+        plt.plot(xdaily,ydaydeathjkt,'y',label="Meninggal Harian DKI Jakarta")
+        plt.xlabel('Tanggal')
+        plt.ylabel('Jumlah Meninggal Harian')
+        plt.legend()
+        st.pyplot(fig2)
