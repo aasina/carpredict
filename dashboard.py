f4075b548958bb64248ac31df2850debb9fb0ed7
Mochamad Sulaeman <MSulaeman@oceaneering.com>
2022-03-22 21:07:34 +0700

    update layout

--- dashboard.py
+++ dashboard.py
@@ -4,11 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#set wide page layout
+# set wide page layout
 st.set_page_config(layout="wide")
 
-#Import Dataset
-dataset = pd.read_csv('datasetcovidsamplejkt.csv')
+# Import Dataset
+dataset = pd.read_csv("datasetcovidsamplejkt.csv")
 
 ydayposid = dataset.ID_DAILY_POSITIVE
 ydayposjkt = dataset.JKT_DAILY_POSITIVE
@@ -16,33 +16,34 @@ ydaydeathid = dataset.ID_DAILY_DEATH
 ydaydeathjkt = dataset.JKT_DAILY_DEATH
 xdaily = dataset.Tanggal
 
-st.sidebar.text('Testing')
+# sidebar
+st.sidebar.text("Testing")
 
-#create first line
+# create first line
 with st.container():
-    st.title('Dashboard Rekapitulasi Data Covid')
+    st.title("Dashboard Rekapitulasi Data Covid")
 
 with st.container():
-    col1, col2 = st.columns(2)
+    col1, col2 = st.columns([2, 1])
 
     with col1:
         fig1 = plt.figure()
-        plt.plot(xdaily,ydayposid,'r',label="Positive Harian Indonesia")
-        plt.plot(xdaily,ydayposjkt,'b',label="Positif Harian DKI Jakarta")
-        plt.xticks([1,150,300,450,600,730], fontsize=6)
+        plt.plot(xdaily, ydayposid, "r", label="Positive Harian Indonesia")
+        plt.plot(xdaily, ydayposjkt, "b", label="Positif Harian DKI Jakarta")
+        plt.xticks([1, 150, 300, 450, 600, 730], fontsize=6)
         plt.yticks(fontsize=6)
-        plt.xlabel('Tanggal')
-        plt.ylabel('Jumlah Positif Harian')
+        plt.xlabel("Tanggal")
+        plt.ylabel("Jumlah Positif Harian")
         plt.legend(fontsize=8)
         st.pyplot(fig1)
 
     with col2:
         fig2 = plt.figure()
-        plt.plot(xdaily,ydaydeathid,'g',label="Meninggal Harian Indonesia")
-        plt.plot(xdaily,ydaydeathjkt,'y',label="Meninggal Harian DKI Jakarta")
-        plt.xticks([1,150,300,450,600,730], fontsize=6)
+        plt.plot(xdaily, ydaydeathid, "g", label="Meninggal Harian Indonesia")
+        plt.plot(xdaily, ydaydeathjkt, "y", label="Meninggal Harian DKI Jakarta")
+        plt.xticks([1, 150, 300, 450, 600, 730], fontsize=6)
         plt.yticks(fontsize=6)
-        plt.xlabel('Tanggal')
-        plt.ylabel('Jumlah Meninggal Harian')
+        plt.xlabel("Tanggal")
+        plt.ylabel("Jumlah Meninggal Harian")
         plt.legend(fontsize=8)
         st.pyplot(fig2)
